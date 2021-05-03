7e4baf0c8cef41a2f06d9b3e6fda28808a140860
Yuta Ide <sadness.ojisan@gmail.com>
2021-05-04 02:59:11 +0900

    error handling (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    error, post, get,web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use serde::Deserialize;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Deserialize)]
+struct Save {
+    winner_id: i32,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[post("/save")]
+async fn greet(save: web::Json<Save>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}!", save.winner_id))
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/battle")]
+async fn battle() -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello A!"))
+}
+
+fn json_error_handler(err: error::JsonPayloadError, _req: &HttpRequest) -> error::Error {
+    use actix_web::error::JsonPayloadError;
+
+    let detail = err.to_string();
+    let resp = match &err {
+        JsonPayloadError::ContentType => {
+            HttpResponse::UnsupportedMediaType().body(detail)
+        }
+        JsonPayloadError::Deserialize(json_err) if json_err.is_data() => {
+            HttpResponse::UnprocessableEntity().body(detail)
+        }
+        _ => HttpResponse::BadRequest().body(detail),
+    };
+    error::InternalError::from_response(err, resp).into()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(greet).service(battle).app_data(
+            web::JsonConfig::default()
+                // register error_handler for JSON extractors.
+                .error_handler(json_error_handler),
+        )
     })
-    .bind("0.0.0.0:8080")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
